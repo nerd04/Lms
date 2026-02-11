3608c930c9133f274453a645cd225c4cff35aec2
Md kaif <mdkaifshaik11196@gmail.com>
2026-02-11 20:58:58 +0530

    inserted route for profile update

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,3 +1,4 @@
+import uploadOnCloudinary from '../config/cloudinary.js';
 import {User} from '../models/userModel.js'
 
 export const getCurrentUser = async (req, res) =>{
@@ -10,5 +11,25 @@ export const getCurrentUser = async (req, res) =>{
     } catch (error) {
         return res.status(500).json({message: `getCurrentUser error ${error}`})
         
+    }
+}
+
+export const updateProfile = async (req, res)=>{
+    try {
+        const userId = req.userId
+        const {name} = req.body;
+        let imageUrl;
+        if(req.file){
+            imageUrl = await uploadOnCloudinary(req.file.path);
+        }
+        const user = await User.findByIdAndUpdate(userId, {name, imageUrl})
+
+        if(!user){
+            return res.status(404).json({message: "user not found"})
+        }
+        await user.save()
+        return res.status(200).json(user)
+    } catch (error) {
+        return res.status(500).json({message: `profile update error: ${error}`})
     }
 }
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import {authUser} from '../middlewares/checkAuth.js'
-import {getCurrentUser} from '../controllers/userController.js'
+import {getCurrentUser, updateProfile} from '../controllers/userController.js'
+import upload from '../middlewares/multer.js'
 
 const userRouter = express.Router()
 
 userRouter.get("/getcurrentuser", authUser, getCurrentUser)
+userRouter.post("/updateprofile", authUser, upload.single("imageUrl"), updateProfile)
 
 export default userRouter
