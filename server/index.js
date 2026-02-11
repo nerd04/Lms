72b8633ce8af639879db0fffb5ba4ba8b9db87a2
Md kaif <148319595+nerd04@users.noreply.github.com>
2026-02-12 00:30:10 +0530

    Update CORS origin to specific frontend URL

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: `${process.env.FRONTEND_URL}`,
+    origin: "https://lms-uby5.vercel.app/",
     credentials: true
 }))
 
