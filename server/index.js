93698131c778db91edaa84c9890ea4fdfb8aaa7f
Md kaif <mdkaifshaik11196@gmail.com>
2026-07-29 16:29:13 +0530

    prodction ready

--- server/index.js
+++ server/index.js
@@ -12,9 +12,22 @@ const app = express();
 
 app.use(express.json())
 app.use(cookieParser())
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:3000",
+  process.env.FRONTEND_URL
+].filter(Boolean);
+
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
 }))
 
 let isConnected = false;
